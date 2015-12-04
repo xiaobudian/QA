e47579e1600ca0f62d5219cc7e3c4992b87bf639
刘帅 <849351660@qq.com>
2015-12-04 18:45:28 +0800

    bug ''

--- Application/Home/Controller/AskController.class.php
+++ Application/Home/Controller/AskController.class.php
@@ -41,7 +41,7 @@ class AskController extends BaseController {
             $filter = $_POST[ 'filter' ];
             $ids = $_POST[ 'ids' ];
 
-            $map[ 'name' ] = array('LIKE', '%' . $filter . '%');
+            $map[ 'name' ] = array('LIKE', '%'.$filter.'%');
             $map[ 'id' ] = array('not in', $ids);
             $tags = M('tag')
 //                ->fetchSql(true)
@@ -52,20 +52,21 @@ class AskController extends BaseController {
             //echo $tags;
             $html = '<div class="tag-suggestions" style = "position: absolute; left: 0px; top: 65px; width: 648px;" >';
             foreach ($tags as $t) {
-                $html = $html . '<div tabindex = "103" style = "width: 206px;" onclick="selecttag(' . $t[ 'id' ] . ',' . $t[ 'name' ] . ')" >
+                $html = $html.'<div tabindex = "103" style = "width: 206px;"
+                                        onclick="selecttag('.$t[ 'id' ].',\''.$t[ 'name' ].'\')" >
                                     <span class="post-tag" >
-                                        <span class="match" >' . $t[ 'name' ] . '</span >
+                                        <span class="match" >'.$t[ 'name' ].'</span >
                                     </span >
-                                    <span class="item-multiplier" > × &nbsp;' . $t[ 'reputation' ] . ' </span >
-                                    <p >' . $t[ 'description' ] . '</p >
+                                    <span class="item-multiplier" > × &nbsp;'.$t[ 'reputation' ].' </span >
+                                    <p >'.$t[ 'description' ].'</p >
                                     <p class="more-info" >
-                                        <a href = "/index.php/Home/Tag/details/id/' . $t[ 'id' ] . '" target = "_blank" > learn more </a >
+                                        <a href="/index.php/Home/Tag/details/id/'.$t[ 'id' ].'" target="_blank"> learn more </a>
                                     </p >
                                  </div >';
             }
 
 
-            $html = $html . ' </div><span class="edit-field-overlay">at least one tag such as (javascript forms image), max 5 tags    </span>';
+            $html = $html.' </div><span class="edit-field-overlay">at least one tag such as (javascript forms image), max 5 tags    </span>';
 
             echo $html;
         }
