5d7be92119c7a4f30226a3949231aa62e0c1d012
刘帅 <849351660@qq.com>
2015-12-29 09:15:56 +0800

    数据库分页查询优化

--- Application/Home/Controller/QuestionController.class.php
+++ Application/Home/Controller/QuestionController.class.php
@@ -13,36 +13,18 @@ use Think\Controller;
 use Think\Exception;
 use Think\Page;
 
-class QuestionController extends BaseController {
+class QuestionController extends BaseController
+{
 
-    public function index($p = 1) {
-
+    public function index($p = 1)
+    {
         $question = M('question');
         $count = $question->count();
         $page = new Page($count, C('PAGESIZE'));
         $show = $page->show();
-        $key = 'question:'.'newest'.$p;
-        $mem = Memcached::getInstance();
-        $questions = $mem->get($key);
-
-        if (!$questions) {
-            $questions =
-                M('question q')
-                    ->order(' q.votes desc ')
-                    ->join(' auth_user u on q.user_id= u.id ')
-                    ->join(' profile p on u.id = p.user_id ')
-                    ->limit($page->firstRow.','.$page->listRows)
-                    ->field('q.id,q.title,q.votes,q.answers, q.content,q.summary,q.views,q.ct,p.pic,p.reputation,u.username,q.user_id')
-                    ->select();
-            $count = count($questions);
-            for ($i = 0; $i < $count; $i++) {
-
-                $tags = $this->getQuestionTags($questions[ $i ][ 'id' ]);
-
-                $questions[ $i ][ 'tags' ] = $tags;
-            }
-            $mem->set($key, $questions);
-        }
+        $query = "call proc_question_page_votes_desc($page->firstRow,$page->listRows) ";
+        $questions = M('question')->query($query);
+        var_dump($questions);
 
         $this->assign('page', $show);
         $this->assign('questions', $questions);
@@ -50,17 +32,18 @@ class QuestionController extends BaseController {
 
     }
 
-    public function details($id) {
-        $map[ 'q.id' ] = array('eq', $id);
+    public function details($id)
+    {
+        $map['q.id'] = array('eq', $id);
         $q = M('question q')->where($map)->join('auth_user u on q.user_id= u.id')
             ->field('q.id,q.title,q.votes,q.content,q.answers,q.views,q.ct,u.username,q.user_id,q.favorite')->select();
         if (hadLogin()) {
             unset($map);
-            $map[ 'user_id' ] = array('eq', getUserId());
-            $map[ 'question_id' ] = array('eq', $id);
+            $map['user_id'] = array('eq', getUserId());
+            $map['question_id'] = array('eq', $id);
             $qv = M('qvote')->where($map)->find();
             if ($qv) {
-                $this->assign('vote_type', $qv[ 'vote_type' ]);
+                $this->assign('vote_type', $qv['vote_type']);
             }
             $fq = M('fquestion')->where($map)->find();
             if ($fq) {
@@ -68,11 +51,11 @@ class QuestionController extends BaseController {
             }
         }
         if ($q) {
-            $q = $q[ 0 ];
+            $q = $q[0];
             $tags = $this->getQuestionTags($id);
-            $q[ 'tags' ] = $tags;
+            $q['tags'] = $tags;
 
-            $mapanswer[ 'question_id' ] = array('eq', $q[ 'id' ]);
+            $mapanswer['question_id'] = array('eq', $q['id']);
             $answers = M('answer a')
                 ->where($mapanswer)
                 ->order('votes desc')
@@ -81,32 +64,34 @@ class QuestionController extends BaseController {
                 ->select();
             if (hadLogin() && $answers) {
                 foreach ($answers as &$a) {
-                    $mapav[ 'answer_id' ] = array('eq', $a[ 'id' ]);
-                    $mapav[ 'user_id' ] = array('eq', getUserId());
+                    $mapav['answer_id'] = array('eq', $a['id']);
+                    $mapav['user_id'] = array('eq', getUserId());
                     $av = M('avote')->where($mapav)->find();
                     if ($av) {
-                        $a[ 'vote_type' ] = $av[ 'vote_type' ];
+                        $a['vote_type'] = $av['vote_type'];
                     }
                     unset($mapav);
                 }
             }
-            $q[ 'q_answers' ] = $answers;
+            $q['q_answers'] = $answers;
 
             $this->assign('q', $q);
             $this->display();
         }
     }
 
-    function getQuestionTags($qid) {
+    function getQuestionTags($qid)
+    {
         return M('tag t')
             ->join('question_tags qt on t.id = qt.tag_id')
-            ->where('qt.question_id = '.$qid)
+            ->where('qt.question_id = ' . $qid)
             ->field('t.id,t.name')
             ->select();
     }
 
-    public function tagged($id, $p = 1) {
-        $question = M('question q')->join(' question_tags qt on q.id = qt.question_id ')->where(' qt.tag_id = '.$id);
+    public function tagged($id, $p = 1)
+    {
+        $question = M('question q')->join(' question_tags qt on q.id = qt.question_id ')->where(' qt.tag_id = ' . $id);
 
         $count = $question->count();
         $page = new \Think\Page($count, C('PAGESIZE'));
@@ -116,17 +101,17 @@ class QuestionController extends BaseController {
             //->fetchSql(true)
             ->join(' question_tags qt on q.id = qt.question_id ')
             ->join('auth_user u on q.user_id = u.id')
-            ->where(' qt.tag_id = '.$id)
+            ->where(' qt.tag_id = ' . $id)
             ->order('q.votes desc')
-            ->limit($page->firstRow.','.$page->listRows)
+            ->limit($page->firstRow . ',' . $page->listRows)
             ->field('q.id,q.title,q.votes,q.answers,q.views,q.ct,u.username,q.user_id')
             ->select();
         $count = count($questions);
         for ($i = 0; $i < $count; $i++) {
 
-            $tags = $this->getQuestionTags($questions[ $i ][ 'id' ]);
+            $tags = $this->getQuestionTags($questions[$i]['id']);
 
-            $questions[ $i ][ 'tags' ] = $tags;
+            $questions[$i]['tags'] = $tags;
         }
 
         $this->assign('page', $show);
@@ -134,9 +119,10 @@ class QuestionController extends BaseController {
         $this->display('index');
     }
 
-    public function answer() {
+    public function answer()
+    {
         if (IS_POST) {
-            $answer_str = $_POST[ 'answer' ];
+            $answer_str = $_POST['answer'];
             $answer_str = urldecode($answer_str);
             $length = strlen($answer_str);
             if ($length > 5) {
@@ -146,11 +132,11 @@ class QuestionController extends BaseController {
                     $answer->answer = $answer_str;
                     $answer->votes = 0;
                     $answer->ct = date('Y-m-d H:i:s');
-                    $answer->question_id = $_POST[ 'question_id' ];
-                    $user = $_SESSION[ 'user' ][ 0 ];
-                    $answer->user_id = $user[ 'id' ];
+                    $answer->question_id = $_POST['question_id'];
+                    $user = $_SESSION['user'][0];
+                    $answer->user_id = $user['id'];
                     $added = $answer->add();
-                    $map[ 'id' ] = array('eq', $_POST[ 'question_id' ]);
+                    $map['id'] = array('eq', $_POST['question_id']);
                     $question = M('question');
                     $inced = $question->where($map)->setInc('answers', 1);
                     if ($added && $inced) {
@@ -169,19 +155,20 @@ class QuestionController extends BaseController {
             }
         }
 
-        $this->redirect('/Home/Question/details/id/'.$_POST[ 'question_id' ]);
+        $this->redirect('/Home/Question/details/id/' . $_POST['question_id']);
     }
 
-    function vote($vote_type, $add = false) {
+    function vote($vote_type, $add = false)
+    {
         $this->checkAuth();
 
         $Question = M('question');
         $Question->startTrans();
 
         $uid = getUserId();
-        $qid = $_POST[ 'id' ];
-        $map[ 'user_id' ] = array('eq', $uid);
-        $map[ 'question_id' ] = array('eq', $qid);
+        $qid = $_POST['id'];
+        $map['user_id'] = array('eq', $uid);
+        $map['question_id'] = array('eq', $qid);
         //删除相关记录
         M('qvote')->where($map)->delete();
 
@@ -196,43 +183,48 @@ class QuestionController extends BaseController {
             $r1 = $qv->add();
         }
         //统计
-        $mapqv[ 'question_id' ] = array('eq', $qid);
-        $mapqv[ 'vote_type' ] = array('eq', VOTEUP);
+        $mapqv['question_id'] = array('eq', $qid);
+        $mapqv['vote_type'] = array('eq', VOTEUP);
         $vote_up_count = M('qvote')->where($mapqv)->count();
-        $mapqv[ 'vote_type' ] = array('eq', VOTEDOWN);
+        $mapqv['vote_type'] = array('eq', VOTEDOWN);
         $vote_down_count = M('qvote')->where($mapqv)->count();
         $count = $vote_up_count - $vote_down_count;
-        $Question->where(' id = '.$qid)->setField('votes', $count);
+        $Question->where(' id = ' . $qid)->setField('votes', $count);
 
 
         if ($r1) {
             $Question->commit();
-            $result[ 'result' ] = true;
-            $result[ 'votes' ] = $count;
+            $result['result'] = true;
+            $result['votes'] = $count;
         } else {
             $Question->rollback();
-            $result[ 'result' ] = false;
+            $result['result'] = false;
         }
         echo json_encode($result);
     }
 
-    public function  voteupon() {
+    public function  voteupon()
+    {
         $this->vote(VOTEUP, true);
     }
 
-    public function  voteupoff() {
+    public function  voteupoff()
+    {
         $this->vote(VOTECANCEL);
     }
 
-    public function  votedownon() {
+    public function  votedownon()
+    {
         $this->vote(VOTEDOWN, true);
     }
 
-    public function  votedownoff() {
+    public function  votedownoff()
+    {
         $this->vote(VOTECANCEL);
     }
 
-    function basefavorite($add = false) {
+    function basefavorite($add = false)
+    {
         $this->checkAuth();
         $r0 = true;
         $r1 = true;
@@ -241,9 +233,9 @@ class QuestionController extends BaseController {
         $fq->startTrans();
 
         $userid = getUserId();
-        $qid = $_POST[ 'id' ];
-        $map[ 'user_id' ] = array('eq', $userid);
-        $map[ 'question_id' ] = array('eq', $qid);
+        $qid = $_POST['id'];
+        $map['user_id'] = array('eq', $userid);
+        $map['question_id'] = array('eq', $qid);
         $r0 = $fq->where($map)->delete();
         if ($add) {
             $f = M('fquestion');
@@ -255,26 +247,28 @@ class QuestionController extends BaseController {
                 ->add();
         }
         $q = M('question');
-        $count = $fq->where(' question_id = '.$qid)->count();
-        $r2 = $q->where(' id = '.$qid)
+        $count = $fq->where(' question_id = ' . $qid)->count();
+        $r2 = $q->where(' id = ' . $qid)
             //->fetchSql(true)
             ->setField('favorite', $count);
         if ($r1 && $r2) {
             $fq->commit();
-            $result[ 'result' ] = true;
-            $result[ 'favorite' ] = $count;
+            $result['result'] = true;
+            $result['favorite'] = $count;
         } else {
             $fq->rollback();
-            $result[ 'result' ] = false;
+            $result['result'] = false;
         }
         echo json_encode($result);
     }
 
-    public function favorite() {
+    public function favorite()
+    {
         $this->basefavorite(true);
     }
 
-    public function unfavorite() {
+    public function unfavorite()
+    {
         $this->basefavorite();
     }
 }
